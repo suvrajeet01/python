52c22a38b1a1e07115cf56597053557ba5ef2630
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2020-05-02 21:46:29 +0530

    11.56.21

--- 11.56.21_1920.1080_1.35G/15.py
+++ 11.56.21_1920.1080_1.35G/15.py
@@ -92,3 +92,24 @@ def pattern(n):
         print('\r')
 
 pattern(int(input('Enter a number')))
+
+#hourglass pattern
+
+def pattern(n):
+    k = n - 2
+    for i in range(n,-1,-1):                #the third parameter is step or the points to be skipped between each points from initial to final position
+        for j in range(k,0,-1):
+            print(end=' ')
+        k +=1
+        for j in range(0,i+1):
+            print('* ',end='')
+        print('\r')
+    k = 2 * n - 2              #bs- blank spaces variable
+    for i in range(0,n):        #loop for outer rows
+        for j in range(0,k):   #loop for columns
+            print(end = ' ')
+        k = k - 1             #decrementing spaces
+        for j in range(0,i+1):
+            print('* ', end='')
+        print('\r')
+pattern(int(input('Enter a number')))
