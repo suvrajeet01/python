0b69566c9991c8dd301317899c0fd858c3745373
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2020-05-05 15:31:35 +0530

    11.56.21

--- 11.56.21_1920.1080_1.35G/15.py
+++ 11.56.21_1920.1080_1.35G/15.py
@@ -280,3 +280,60 @@ def pattern(n):
             x = x+1
         print('\r')
 pattern(int(input('Enter a number')))
+
+#K shaped character pattern
+for i in range(7):
+    for j in range(7):
+        if j == 0 or i-j == 3 or i+j ==3:
+            print('A',end = '')
+        else:
+            print(end = ' ')
+    print()
+print()
+for i in range(7):
+    for j in range(7):
+        if j == 0 or i-j == 3 or i+j ==3:
+            print('*',end = '')
+        else:
+            print(end = ' ')
+    print()
+
+#triangle pattern program
+def pattern(n):
+    k = 2 * n - 2
+    x = 65
+    for i in range(0,n):
+        ch = chr(x)
+        x = x + 1
+        for j in range(0,k):
+            print(end=' ')
+        for j in range(0,i+1):
+            print(ch,end = ' ')
+        print('\r')
+pattern(int(input('Enter a number')))
+
+#diamond shaped pattern with characters
+def pattern(n):
+    k = 2 * n - 2
+    x = 65
+    for i in range(0,n):
+        ch = chr(x)
+        x = x + 1 
+        for j in range(0,k):
+            print(end = ' ')
+        k = k - 1
+        for j in range(0,i+1):
+            print(ch,end=' ')
+        print('\r')
+    k = n - 2
+    x = 65
+    for i in range(n,-1,-1):
+        ch = chr(x)
+        x = x +1
+        for j in range(k,0,-1):
+            print(end = ' ')
+        k = k+1
+        for j in range(0,i+1):
+            print(ch,end = ' ')
+        print('\r')
+pattern(int(input('Enter a number')))
