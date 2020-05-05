8a6c8ab58c1b6161be7f23dac1dfbcc8de4c685f
suvrajeet01 <suvrajeetbanerjee34767@gmail.com>
2020-05-05 15:32:17 +0530

    11.56.21

--- 11.56.21_1920.1080_1.35G/tempCodeRunnerFile.py
+++ 11.56.21_1920.1080_1.35G/tempCodeRunnerFile.py
@@ -1,23 +1,18 @@
-#diamond pattern with numbers
 def pattern(n):
-    k = 2 * n - 2
-    x = 0
-    for i in range(0,n):
-        x = x + 1 
-        for j in range(0,k):
-            print(end = ' ')
-        k = k - 1
-        for j in range(0,i+1):
-            print(x,end=' ')
-        print('\r')
     k = n - 2
-    x = 0
-    for i in range(n,-1,-1):
-        x = x +1
+    for i in range(n,-1,-1):                #the third parameter is step or the points to be skipped between each points from initial to final position
         for j in range(k,0,-1):
-            print(end = ' ')
-        k = k+1
+            print(end=' ')
+        k +=1
         for j in range(0,i+1):
-            print(x,end = ' ')
+            print('* ',end='')
+        print('\r')
+    k = 2 * n - 2              #bs- blank spaces variable
+    for i in range(0,n+1):        #loop for outer rows
+        for j in range(0,k):   #loop for columns
+            print(end = ' ')
+        k = k - 1             #decrementing spaces
+        for j in range(0,i+1):
+            print('* ', end='')
         print('\r')
 pattern(int(input('Enter a number')))
